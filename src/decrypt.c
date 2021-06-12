176de84ce4da259ae84716264097c980599f7086
Tamara <tamypuig@gmail.com>
2021-06-11 21:40:25 -0300

    trying decrypt

--- src/decrypt.c
+++ src/decrypt.c
@@ -1,79 +1,63 @@
 #include "cript.h"
 #include <stdio.h>
-#include <dirent.h>
-#include <sys/types.h>
-#include <errno.h>
 #include <string.h>
 
-int input_recover(){
-    DIR* FD;
-    struct dirent* in_file;
-    FILE    *entry_file;
-    /* Scanning the in directory */
-    printf("%s\n",crypt_info.args.input);
-    if (NULL == (FD = opendir (crypt_info.args.input))) 
-    {
-        fprintf(stderr, "Error : Failed to open input directory - %s\n", strerror(errno));
-        return 1;
-    }
-    int dirLen = strlen(crypt_info.args.input)+2;
-    int file_count = 0;
-    while ((in_file = readdir(FD))) 
-    {
-        if(file_count == crypt_info.args.k){
-            fprintf(stderr,"La cantidad de archivos en el directorio es incorrecta.\n");
-            fprintf(stderr,"Se esperaban %d.\n",crypt_info.args.k);
-            closedir(FD);
-            return EXIT_FAILURE;
-        }
-        if (!strcmp (in_file->d_name, "."))
-            continue;
-        if (!strcmp (in_file->d_name, ".."))    
-            continue;
-        
-        dirLen += strlen(in_file->d_name)+strlen("/");
-        char path[dirLen];
-        path[0]=0;
-        strcat(path,crypt_info.args.input);
-        strcat(path,"/");
-        strcat(path,in_file->d_name);
+int input_recover() {
 
-         if ((entry_file = fopen(path, "rb")) == NULL){
-            fprintf(stderr,"error al abrir el archivo %s\n",in_file->d_name);
-            closedir(FD);
-            return EXIT_FAILURE;
-        }
-        uint8_t data[HEADER_SIZE];
-        if (fread(&data, HEADER_SIZE, 1, entry_file) < 1){
-            /* Couldn't read the file header - return NULL... */
-            fclose(entry_file);
-            closedir(FD);
-            fprintf(stderr,"error al leer el archivo %s\n",in_file->d_name);
-            return EXIT_FAILURE;
-        }
-        if(parse(&crypt_info.shadows[file_count++].header,data) == ERROR){
-            exit(EXIT_FAILURE);
-        }
-        if(crypt_info.args.verbose){
-            printf("\nArchivo leído: %s\n",in_file->d_name);
-            printHeaderInfo(&crypt_info.shadows[file_count-1].header);
-        }
-        fclose(entry_file);
+    if (find_images_in_directory(crypt_info.args.input) == EXIT_FAILURE) {
+        return EXIT_FAILURE;
     }
-    closedir(FD);
 
-    if(crypt_info.args.k != file_count){
-         fprintf(stderr,"La cantidad de archivos en el directorio es incorrecta.\n");
-            fprintf(stderr,"Se esperaban %d.\n",crypt_info.args.k);
-            return EXIT_FAILURE;
-    }
+    secret_image *image = &crypt_info.secret;
+    image->header_data = (uint8_t *)malloc(crypt_info.shadows[0].header.offset * sizeof(uint8_t));
+    image->image_data = (uint8_t *)malloc(crypt_info.shadows[0].header.width_px * crypt_info.shadows[0].header.height_px * sizeof(uint8_t));
+    strcpy(image->filename, crypt_info.args.output);
+
+    memcpy(&image->header,&crypt_info.shadows[0].header,sizeof(crypt_info.shadows[0].header));
+    memcpy(image->header_data,crypt_info.shadows[0].header_data,crypt_info.shadows[0].header.offset);
+    
     return EXIT_SUCCESS;
 }
-int decryption_recover(){
-    printf("estoy en encrypt recover\n");
+
+int decryption_recover() {
+    int height = crypt_info.shadows[0].header.height_px;
+    int width = crypt_info.shadows[0].header.width_px;
+    int k = crypt_info.args.k;
+    uint8_t inv_prod[256];
+    generate_galois_inverse_table(inv_prod,256);
+
+    int bcount = (height * width) / k;
+    for (int i = 0; i < bcount; i++) {
+        uint8_t x[k];
+        uint8_t f_x[k];
+        for (int j = 0; j < k; j++){
+            uint8_t aux[4];
+            get_block_by_index(&crypt_info.shadows[j], i, aux);
+
+            uint8_t t;
+
+            t = (aux[1] & 0x07) << 5;
+
+            t |= (aux[2] & 0x07) << 2;
+
+            t |= (aux[3] & 0x03);
+
+            //uint8_t parity = (aux[3] & 0x04) >> 2;
+            x[j] = aux[0];
+            f_x[j] = t;
+        }
+        uint8_t secret[k];
+        interpolation(x,f_x,secret,k,inv_prod);
+        memcpy(crypt_info.secret.image_data+i*k,secret,k*sizeof(uint8_t));
+    }
+
     return EXIT_SUCCESS;
 }
-int output_recover(){
+
+
+int output_recover() {
     printf("estoy en output recover\n");
-    return EXIT_SUCCESS;
+    int return_value = write_secret_image(crypt_info.secret.filename, &crypt_info.secret);
+    free_all_images();
+    return return_value;
 }
--- src/library.c
+++ src/library.c
@@ -28,6 +28,47 @@ void free_image(int image_index) {
     }
 }
 
+int find_images_in_directory (char * path){
+    DIR * dir;
+    struct dirent * dirp;
+    int read_images = 0;
+
+    if ((dir = opendir(path)) == NULL) {
+        fprintf(stderr, "Error reading directory %s\n", path);
+        return EXIT_FAILURE;
+    }
+
+    char cwd[MAX_PATH_LENGHT];
+
+    if (getcwd(cwd, sizeof(cwd)) == NULL) {
+        fprintf(stderr, "Error accessing current working directory %s\n", path);
+        return EXIT_FAILURE;
+    }
+
+    chdir(path);
+
+    while((dirp = readdir(dir))!=NULL && read_images < crypt_info.args.k) {
+        if(dirp->d_type == 8) {
+            if(crypt_info.args.verbose) {
+                printf("Analyzing if file \"%s\" is suitable for being a shadow\n", dirp->d_name);
+            }
+            if (read_image(dirp->d_name, read_images) == EXIT_SUCCESS) {
+                read_images++;
+            };
+        }
+    }
+
+    chdir(cwd);
+    closedir(dir);
+
+    if (read_images < crypt_info.args.k) {
+        fprintf(stderr, "There are not enough shadow images in the given directory\n");
+        return EXIT_FAILURE;
+    }
+    
+    return EXIT_SUCCESS;
+}
+
 int read_image(char * path, int image_index) {
 
     FILE * fp;
@@ -60,6 +101,10 @@ int read_image(char * path, int image_index) {
     width = image->header.width_px;
     offset = image->header.offset;
 
+    if(image_index != 0 && (height != crypt_info.shadows[0].header.height_px || width != crypt_info.shadows[0].header.width_px)){
+        fprintf(stderr, "Las imagenes no tienen el mismo tamaño.\n");
+        return EXIT_FAILURE;
+    }
     image->header_data = (uint8_t *) malloc(offset * sizeof(uint8_t));
     
     image->image_data = (uint8_t **) malloc(height * sizeof(uint8_t *));
@@ -176,7 +221,27 @@ int write_image(char * file_path, bmp_image * image) {
     return EXIT_SUCCESS;
 }
 
-// magia de Coda? No no, stackoverflow 
+int write_secret_image(char * file_path, secret_image * image) {
+    FILE * fp;
+
+    int height = image->header.height_px;
+    int width = image->header.width_px;
+    int offset = image->header.offset;
+
+    if ((fp = fopen(file_path, "wb")) == NULL){
+        fprintf(stderr,"Error al escribir archivo %s\n", file_path);
+        return EXIT_FAILURE;
+    }
+
+    fwrite(image->header_data, sizeof(uint8_t), offset, fp);
+    fwrite(image->image_data, sizeof(uint8_t), width * height, fp);
+
+    fclose(fp);
+
+    return EXIT_SUCCESS;
+}
+
+
 uint8_t calcParityBit(uint8_t x){
     return ( (x>>7) ^ 
              (x>>6) ^
@@ -210,8 +275,68 @@ void write_block_by_index (bmp_image * image, int index, uint8_t * data) {
     image->image_data[row-1][col+1] = data[3];
 }
 
-// Fila = 2*(index/(W/2)) -> División entera me da el cociente, multiplico por dos y me da la fila donde está
+void generate_galois_inverse_table (uint8_t * array, int n) {
+    uint8_t aux = 0;
+    uint8_t i, j;
+    bool flag = false;
 
-// Columna = 2*(index%(W/2)) -> El resto me da la columna, multiplico por dos y me da el byte más a la izquierda
+    array[0] = 0;
 
-// Fila Invertida = H - 1 - 2*(index/(W/2))
+    for (i = 1; i != 0; i++) {
+        for(j = 1; j != 0 && !flag; j++) {
+            aux = prod(i, j);
+            if(aux == 1) {
+                array[i] = j;
+                flag = true;
+            }
+        }
+        flag = false; 
+    }
+
+}
+
+void interpolation(uint8_t * x, uint8_t * y, uint8_t * s,int k, uint8_t * inv) {
+    uint8_t actual_secret = 0, productoria, resta, div, mult, y_prima;
+    int i, j, r;
+    
+    for(i = 0; i < k; i++){
+        productoria = 1;
+
+        for(j = 0; j < k; j++){
+            if(j != i){
+                resta = sum(x[i],x[j]);
+                div = prod(x[j],inv[resta]);
+                productoria = prod(productoria,div);
+            }
+        }    
+
+        mult = prod(y[i],productoria);
+        actual_secret = sum(actual_secret,mult);
+    }
+
+    s[0] = actual_secret;
+    
+    for(r = 1; r<k; r++) {
+        actual_secret = 0;
+
+        for(i = 0; i <= k-r; i++){
+            productoria = 1;
+
+            for(j = 0; j <= k-r; j++){
+                if(j != i){
+                    resta = sum(x[i],x[j]);
+                    div = prod(x[j],inv[resta]);
+                    productoria = prod(productoria,div);
+                }
+            }  
+              
+            y_prima = sum(y[i],s[0]);
+            y_prima = prod(y_prima,inv[x[i]]);
+            
+            mult = prod(y_prima,productoria);
+            actual_secret = sum(actual_secret,mult);
+        }
+        
+        s[r] = actual_secret;
+    }
+}
