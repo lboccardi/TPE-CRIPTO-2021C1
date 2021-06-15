420b597c70f478dfe018f5d80036d9bfcaee06ab
Lboccardi <lboccardi@itba.edu.ar>
2021-06-14 21:14:46 -0300

    Fixed code identation

--- src/encrypt.c
+++ src/encrypt.c
@@ -2,8 +2,7 @@
 #include <stdio.h>
 #include <string.h>
 
-
-int input_distribute(){
+int input_distribute() {
 
     if (read_image_secret(crypt_info.args.input) == EXIT_FAILURE) {
         return EXIT_FAILURE;
@@ -16,45 +15,43 @@ int input_distribute(){
     return EXIT_SUCCESS;
 }
 
-
-int encryption_distribute(){
+int encryption_distribute() {
     int height = crypt_info.secret.header.height_px;
     int width = crypt_info.secret.header.width_px;
     int k = crypt_info.args.k;
     int n = crypt_info.n;
-    
-    int bcount = (height*width)/k;
-        for(int i=0; i<bcount;i++){
-            uint8_t * secret = crypt_info.secret.image_data+k*i;
-            uint8_t xs[n], block[k];
-            memcpy(block,secret,k*sizeof(uint8_t));
 
-         for(int j = 0; j < n ;j++) {
-             uint8_t aux[4];
-             get_block_by_index(&crypt_info.shadows[j],i,aux);
+    int bcount = (height * width) / k;
+    for (int i = 0; i < bcount; i++) {
+        uint8_t *secret = crypt_info.secret.image_data + k * i;
+        uint8_t xs[n], block[k];
+        memcpy(block, secret, k * sizeof(uint8_t));
 
-             for(int r=0;r<j;r++){
-                 if(aux[0]==xs[r]){
-                     aux[0]=(aux[0]+1)%255;
-                     r=0;
-                 }
-             }
+        for (int j = 0; j < n; j++) {
+            uint8_t aux[4];
+            get_block_by_index(&crypt_info.shadows[j], i, aux);
 
-             xs[j]=aux[0];
+            for (int r = 0; r < j; r++) {
+                if (aux[0] == xs[r]) {
+                    aux[0] = (aux[0] + 1) % 255;
+                    r = 0;
+                }
+            }
 
-             distribute_function_in_block(block, k, aux);
-             write_block_by_index(&crypt_info.shadows[j],i,aux);
-         }   
-        }    
+            xs[j] = aux[0];
+
+            distribute_function_in_block(block, k, aux);
+            write_block_by_index(&crypt_info.shadows[j], i, aux);
+        }
+    }
     return EXIT_SUCCESS;
 }
 
-int output_distribute(){
+int output_distribute() {
 
-    bmp_image * image;
+    bmp_image *image;
     int length = strlen(crypt_info.args.output) + MAX_PATH_LENGHT + 2;
     char new_file_name[length];
-
 
     for (int i = 0; i < crypt_info.n; i++) {
         image = &crypt_info.shadows[i];
